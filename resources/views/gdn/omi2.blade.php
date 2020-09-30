a0f1abaf229ecb489111535091ff7c9c7c012ff7
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2020-09-30 20:04:55 +0000

    Mm

---
+++ resources/views/gdn/omi2.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>【話題騒然！？】コロナ仕様の出会い始まる！<title>
+        <link href="{{ secure_asset('css/omi2.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>Best Community</h1>
+        </header>
+        
+        <div class="body">
+            
+            <div class="top1">
+                <h3>【40歳以上が熱い！？】いま話題沸騰の気軽に出会える「オンラインデート」がスゴすぎる！</h3>
+                <div class="line"></div>
+                <p>最終更新日：2020.9.28</p>
+                <img src="{{ asset('image/o_lp_1.jpg') }}"></img>
+            </div>
+            
+            <div class="top1-below">
+                <p><span class="pink-big">パートナーが欲しいなぁ…</span></p>
+                <p>と思ってはいるものの、このご時世、コロナが落ち着いてきたとはいえ、外出するのはまだまだ気がひけるし、家にいるだけでは出会いのチャンスもなかなかない。</p>
+                <p>と、思っていたら…</p>
+                <p>なんと！<br>同僚が<span class="highlight-blue">最近、彼女ができた</span>って騒いでたんです！</p>
+                <img src="{{ asset('image/o_lp_2.JPEG') }}"></img>
+                <p>いやいやいやいや。</p>
+                <p>この<span class="pink">ご時世に出会って、しかもカップル成立まで</span>って。</p>
+                <p>どうやって！？</p>
+                <p>しかも、<span class="black-big">今はデートもできない</span>じゃん。</p>
+                <p>って聞いたら、<span class="pink-bottom">「オンラインデート」</span>してるんだって？</p>
+                <p>えっ、なにそれ！？<br>今、そんなのあるの！？</p>
+                <p>なんでも、<span class="pink">「オンラインでデートができる」アプリ</span>があるらしい…<br>ちょっと気になるじゃん！！</p>
+            </div>
+            
+            <br>
+            
+            <div class="top2">
+                <h3>オンラインデートが予想以上に面白そうな件...</h3>
+                <img src="{{ asset('image/o_lp_5.jpg') }}"></img>
+                <p>気になりすぎて<span class="pink-big">「オンラインデート」</span>について調べてみたんですけど、やっぱり最近って外出の機会が減ってるじゃないですか。</p>
+                <img src="{{ asset('image/o_lp_3.png') }}"></img>
+                <p>そこで、<span class="pink">「オンラインデート」のプランを考える人が増えている</span>らしいんです。</p>
+                <img src="{{ asset('image/o_lp_4.png') }}"></img>
+                <p>でも、<br><span class="pink">「オンラインデート」って言ってもどんなことやるの？</span><br>と思って調べてみると…</p>
+            </div>
+            
+            <div class="top2-below">
+                <div class="title">
+                    <p>ビデオチャットしながら映画を観たり</p>
+                </div>
+                <img src="{{ asset('image/o_lp_6.jpg') }}"></img>
+                <div class="title">
+                    <p>ビデオチャットしながらお酒を飲んだり</p>
+                </div>
+                <img src="{{ asset('image/o_lp_7.jpg') }}"></img>
+                <div class="title">
+                    <p>お寿司好き同士でおしゃべりしたり</p>
+                </div>
+                <img src="{{ asset('image/o_lp_8.jpg') }}"></img>
+                <p>けっこういろいろバリエーションがあるみたいなんですよね。</p>
+                <p><span class="pink">趣味を一緒に語り合ったり</span>、同時に<span class="pink">美味しいものを食べながら映画見たり</span>って、それ普通のデートと一緒じゃん！！</p>
+                <p>実際にオンラインデートをした人たちの満足度も高くて…</p>
+                <img src="{{ asset('image/o_lp_9.png') }}"></img>
+                <p>むしろ</p>
+                <p><span class="highlight-blue-big">普通のデートよりオンラインデートがいい！！</span></p>
+                <p>なんて意見もあるくらい！</p>
+                <p>いいなぁ〜<br>オレもやってみたい！</p>
+                <p><span class="bold-border-bottom">何か良さげなアプリ探してみるか！</span></p>
+            </div>
+            
+            <br>
+            
+            <div class="top3">
+                <h3>オンラインデートするなら「Omiai」がいいらしい！</h3>
+                <img src="{{ asset('image/o_lp_10.jpg') }}"></img>
+                <p>パートナーができたという知り合いに、どうやってオンラインデートしているのか聞いたら、</p>
+                <p><span class="pink-big-border-bottom">「Omiai」</span></p>
+                <p>っていうアプリを使っていることがわかりました。</p>
+                <p>なぜ「Omiai」がおすすめなのかというと…</p>
+                <img src="{{ asset('image/o_lp_11.png') }}"></img>
+                <p>と、とにかく出会える確率はもちろん、<span class="black">サポートに対する安心感も違う</span>らしいんです！</p>
+                <p>しかも！</p>
+                <p><span class="pink">アプリ側で積極的に「オンラインデート」を勧めてくれる</span>から、初めてでもやりやすい！</p>
+                <p>こういうサービスはありがたいですね。特に、<span class="black">自分はあんまりガツガツいけるタイプじゃない</span>のでかなり助かる！</p>
+                <a href="https://t.afi-b.com/visit.php?guid=ON&a=R100461-H3470814&p=37183994" rel="nofollow" target="_blank"><img src="{{ asset('image/o_lp_12.png') }}"></img></a>
+            </div>
+            
+            <br>
+            
+            <div class="top4">
+                <h3>普段の出会いがなくてもしっかりマッチング！これは期待できそう！</h3>
+                <p>実際に利用した人のレポートも読んでみたんですけど、<span class="pink">皆幸せそう！</span></p>
+                <p><span class="pink">初めてマッチングサービスを使った人もうまくいってる</span>っていうのが勇気付けられるなぁ。</p>
+                <img src="{{ asset('image/o_lp_13.png') }}"></img>
+                <img src="{{ asset('image/o_lp_14.png') }}"></img>
+                <img src="{{ asset('image/o_lp_15.png') }}"></img>
+                <p>はぁ〜〜〜羨ましい！</p>
+                <p><span class="pink">毎月10,000人にパートナーが誕生</span>してるらしいので、これならオレも…！<br>って期待しちゃいますよね！！</p>
+                <a href="https://t.afi-b.com/visit.php?guid=ON&a=R100461-H3470814&p=37183994" rel="nofollow" target="_blank"><img src="{{ asset('image/o_lp_12.png') }}"></img></a>
+            </div>
+            
+            <br>
+            
+            <div class="top5">
+                <h3>お試しは無料！始めるなら今がチャンス！？</h3>
+                <img src="{{ asset('image/o_lp_16.jpg') }}"></img>
+                <p><span class="bold-border-bottom">無料で始められるし、セキュリティ面も安心！</span></p>
+                <p>時間がじっくりとれるこんな時だからこそ、<span class="pink">おうちで出会いを探してみる</span>のもいいかも！？</p>
+                <p>繰り返しますが</p>
+                <p><span class="highlight-blue-big">お試しは無料</span></p>
+                <p>なので、暇を持て余しているなら、今こそ始め時なのかも！？</p>
+                <p>暗くなりがちな時は、<span class="pink">素敵なパートナーと明るい話題で盛り上がりたい</span>ですよね！</p>
+                <p>オレも早速始めてみたいと思います！！<br>彼女できたら報告しますね笑</p>
+                <a href="https://t.afi-b.com/visit.php?guid=ON&a=R100461-H3470814&p=37183994" rel="nofollow" target="_blank"><img src="{{ asset('image/o_lp_17.png') }}"></img></a>
+            </div>
+        </div>
+        
+        <br>
+        
+        <footer>
+            <p>copyright @</p>
+        </footer>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('app', function () {
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('gdn.luna'); //獲得用(2)
+    return view('gdn.omi2'); //獲得用(4)
     
+    // return view('gdn.omiai'); //獲得用(3)
+    // return view('gdn.luna'); //獲得用(2)
     // return view('gdn.job'); //獲得用(1)
 });
