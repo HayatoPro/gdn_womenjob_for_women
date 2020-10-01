243d9cdfaedfffbd0bfd7769e5a520c466463bc9
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2020-10-01 02:36:46 +0000

    mm

--- resources/views/gdn/adult.blade.php
+++ resources/views/gdn/adult.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>女子●生が暴露！？今話題のアプリとは…</title>
-        <link href="{{ secure_asset('css/check.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/adult.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
     <body>
