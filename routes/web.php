200805aaba466f49615205e3dda3068912a127a9
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2020-10-13 19:02:42 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,18 @@
 // LP獲得用
 Route::get('app', function () {
     return view('gdn.check'); //提出用
-    // return view('gdn.adult'); //獲得用(5)
+    // return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON') //トークウィズ - Preaf
     
+    // return view('gdn.adult'); //獲得用(5)
     // return view('gdn.luna'); //獲得用(2)
     // return view('gdn.job'); //獲得用(1)
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('gdn.adult'); //獲得用(5)
+    return redirect('https://preaf.jp/pa.do?s=v04031&o=45765&guid=ON') //トークウィズ - Preaf
     
+    // return view('gdn.adult'); //獲得用(5)
     // return view('gdn.omi2'); //獲得用(4)
     // return view('gdn.omiai'); //獲得用(3)
     // return view('gdn.luna'); //獲得用(2)
