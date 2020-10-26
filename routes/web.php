67b91b1b5ebbbd9734d47f595849783b591d7628
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2020-10-26 11:54:29 +0000

    Mm

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // LP獲得用
 Route::get('app', function () {
-    return view('gdn.check'); //提出用
-    // return view('gdn.papa'); //ワクワク - パパ活訴求
+    // return view('gdn.check'); //提出用
+    return view('gdn.papa'); //ワクワク - パパ活訴求
     
     // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16786'); //ワクワク直遷移 - fam
     // return redirect('https://a-trade.jp/redirect/zoo?media=G16531'); //ZOO - Aトレード
