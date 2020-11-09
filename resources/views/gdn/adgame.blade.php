b6f9ff6010fb1868651ddb7f7cbf52433fb6eb6e
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2020-11-09 07:12:59 +0000

    Mm

---
+++ resources/views/gdn/adgame.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>作成中</title>
+        <link href="{{ secure_asset('css/adgame.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <img src="{{ asset('image/anahiro_0.jpg') }}"></img>
+        <div class="button">
+            <a href="https://a-trade.jp/redirect/anahiro?media=G16531" rel="nofollow" class="btn-square-shadow">公式サイトへ <i class="fas fa-external-link-alt"></i></a>
+        </div>
+        <div class="heading">
+            <h1>登録ガイド！</h1>
+        </div>
+        <h2>1. 『入手』ボタンをタップ!</h2>
+        <img src="{{ asset('image/anahiro_1.jpg') }}"></img>
+        <br>
+        <h2>2. 『いますぐ恋を始める』ボタンをタップ!</h2>
+        <img src="{{ asset('image/anahiro_2.jpg') }}"></img>
+        <br>
+        <h2>3. アカウント情報を入力!</h2>
+        <img src="{{ asset('image/anahiro_3.jpg') }}"></img>
+        <br>
+        <h2>4. 『登録』ボタンをタップ!</h2>
+        <img src="{{ asset('image/anahiro_4.jpg') }}"></img>
+        <div class="heading">
+            <h1>たった登録１分で<br>『即抜き』ムービー開始！</h1>
+        </div>
+        <div class="button">
+            <a href="https://a-trade.jp/redirect/anahiro?media=G16531" rel="nofollow" class="btn-square-shadow">公式サイトへ <i class="fas fa-external-link-alt"></i></a>
+        </div>
+    </body>
+</html>
