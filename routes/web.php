dd23273a73d3a1673dd5930639c7abe0e8d4fb70
EC2 Default User <ec2-user@ip-172-31-6-92.us-east-2.compute.internal>
2021-01-29 18:21:04 +0000

    M

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 // LP獲得用
 Route::get('news', function () {
-    // return view('gdn.check'); //提出用
-    return view('gdn.adultand'); //YouTube - アダルトAndroid訴求
+    return view('gdn.check'); //提出用
+    // return view('gdn.adultand'); //YouTube - アダルトAndroid訴求
     
     // return view('gdn.jobNew'); //PoPo訴求
     // return redirect('https://a-trade.jp/redirect/anahiro?media=G16531'); //アナザーヒロイン直遷移 - Aトレード
